e788fd0e13c54529ce457fe994e37ee498353e0f
linusramok <linussunil.dsk@gmail.com>
2024-04-03 02:03:55 +0530

    changing meta data

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
   siteMetadata: {
     title: 'Sunil Kumar Dunna',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      "Sunil Kumar Dunna is a software engineer who builds interactive and responsive web applications using React.",
+    siteUrl: 'https://www.sunilkumardunna.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
   },
   plugins: [
